0b3a53c1b687c8a5f479e030a42281ce9bcb4276
Ally Brannon <allison.brannon@gmail.com>
2020-04-07 09:52:47 -0400

    updated activity component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { eat } from "../redux/actions";
 
-const Activity = (props) => <h1> Activity Component </h1>;
+const Activity = ({ activity, eat }) => (
+  <>
+    <h1> What is the cat doing? </h1>
+    <p>The cat is {activity}</p>
+    <button onClick={() => eat()}>Eating</button>
+  </>
+);
 
-export default Activity;
+const mapStateToProps = (state) => {
+  const { activity } = state;
+  return activity;
+};
+
+export default connect(
+  mapStateToProps, //mapStateToProps - what do i want?
+  { eat } //mapDispatchToProps - what do I want to do?
+)(Activity);
